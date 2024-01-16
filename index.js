571243681da8941ab45fb5ff86f3f1605fae5cc0
yanlong-sun <sunyanlong712@gmail.com>
2024-01-16 22:00:47 +0800

    test

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { config } = require("dotenv");
 require("dotenv").config();
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+const client = new Discord.Client();
+client.once("ready", () => {
+  console.log("Ready! Logged in !");
 });
 
-client.on("messageCreate", async (message) => {
-  // if (message.author.bot) return; // 忽略其他机器人
-  // if (!message.guild) return; // 确保消息来自服务器
-  // const args = message.content.trim().split(/ +/);
-  const args = message.trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-  const content = args.join(" ");
-  if (command === "-update") {
-    console.log(content);
+client.on("message", async (message) => {
+  if (message === "ping") {
+    console.log("pongx");
   }
 });
 
