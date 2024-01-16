4f9d8cc0c44513232a186c9f59a02cf78dc7e005
yanlong-sun <sunyanlong712@gmail.com>
2024-01-16 23:03:57 +0800

    test

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 require("dotenv").config();
+
 const { Client, GatewayIntentBits } = require("discord.js");
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.on("message", async (message) => {
-  console.log(message);
-  console.log(message.content);
-  if (message.content === "ping") {
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  // 确保消息不是来自Bot自身
+  if (message.author.bot) return;
+
+  // 你的其他消息处理逻辑
+  if (message.content === "!ping") {
     message.reply("Pong!");
   }
 });
-
 const args = process.argv.slice(2);
 console.log("token is", args[0]);
 client.login(args[0]);
