8146e5d49889d596671a83c02900c8e2ae3c9618
yanlong-sun <sunyanlong712@gmail.com>
2024-01-17 23:13:09 +0800

    test

--- commands/util/xxoo.js
+++ commands/util/xxoo.js
@@ -3,9 +3,9 @@ const wait = require("node:timers/promises").setTimeout();
 
 module.exports = {
   cooldown: 5,
-  data: new SlashCommandBuilder().setName("xxoo").setDescription(""),
+  data: new SlashCommandBuilder().setName("xxoo").setDescription("xxoo"),
   async execute(interaction) {
-    await interaction.reply("");
+    await interaction.reply("xxoo");
     await wait(3_000);
     await interaction.editReply("xxoo");
   },
