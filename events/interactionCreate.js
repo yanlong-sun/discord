4df648ed5268f9a08447ffd3f747466b80db3dfb
yanlong-sun <sunyanlong712@gmail.com>
2024-01-19 01:55:33 +0800

    test

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -39,15 +39,13 @@ module.exports = {
     if (!interaction.isChatInputCommand()) return;
     const command = interaction.client.commands.get(interaction.commandName);
     if (!command) {
-      console.error(
-        `No command matching ${interaction.commandName} was found.`
-      );
+      logger.error(`No command matching ${interaction.commandName} was found.`);
       return;
     }
     try {
       await command.execute(interaction);
     } catch (error) {
-      console.error(error);
+      logger.error(error);
       if (interaction.replied || interaction.deferred) {
         await interaction.followUp({
           content: "There was an error while executing this command!",
