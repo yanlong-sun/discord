3a62a2aff5ceb5245529c4d2834b0f441637f5e2
yanlong-sun <sunyanlong712@gmail.com>
2024-01-19 02:34:06 +0800

    test

--- commands/work/tv.js
+++ commands/work/tv.js
@@ -11,11 +11,14 @@ module.exports = {
     .setName("tv")
     .setDescription("生成小宝和爱一帆搜索结果"),
   async execute(interaction) {
-    const modal = new ModalBuilder().setCustomId("tvModal").setTitle("搜索");
+    const modal = new ModalBuilder()
+      .setCustomId("tvModal")
+      .setTitle("搜索关键字");
     const keyword = new TextInputBuilder()
       .setCustomId("keyword")
       .setLabel("KeyWord")
       .setPlaceholder("莫蒂和瑞克")
+      .setValue("莫蒂和瑞克")
       .setStyle(TextInputStyle.Short)
       .setRequired(true);
     // action row only holds one text input
