34f207967b232d99742184560192e669f3087ceb
yanlong-sun <sunyanlong712@gmail.com>
2024-01-19 01:51:31 +0800

    test

--- commands/util/xxoo.js
+++ commands/util/xxoo.js
@@ -5,14 +5,7 @@ module.exports = {
   data: new SlashCommandBuilder().setName("xxoo").setDescription("xxoo"),
   async execute(interaction) {
     await interaction.reply(
-      `
-      hey babe, welcome to this channel. \n \
-      I made a bot for us to log our daily tasks more easily. \n \
-      I'm starting to feel like this is kind of silly because I spent the nights of this whole week on it. \n \
-      Try typing **/log**, enter the information, open the Google Sheet, and see what happens. \n \
-      You see this in English because I don't want to be 这么客气的跟你说这些话. \n \
-      Tell me your thoughts, and we can make it better!
-      `
+      "hey babe, welcome to this channel. \n I made a bot for us to log our daily tasks more easily. \n I'm starting to feel like this is kind of silly because I spent the nights of this whole week on it. \n Try typing **/log**, enter the information, open the Google Sheet, and see what happens. \n You see this in English because I don't know how to 用中文这么客气的跟你说这些. \n Tell me your thoughts, and we can make it better!"
     );
   },
 };
