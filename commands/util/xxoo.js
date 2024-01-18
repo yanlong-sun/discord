7213783550d11cf678aa25617596401d9aa479d3
yanlong-sun <sunyanlong712@gmail.com>
2024-01-19 01:45:15 +0800

    test

--- commands/util/xxoo.js
+++ commands/util/xxoo.js
@@ -2,11 +2,17 @@ const { SlashCommandBuilder } = require("discord.js");
 const wait = require("node:timers/promises").setTimeout;
 
 module.exports = {
-  cooldown: 5,
   data: new SlashCommandBuilder().setName("xxoo").setDescription("xxoo"),
   async execute(interaction) {
-    await interaction.reply("xxoo");
-    await wait(3_000);
-    await interaction.editReply("xxoo");
+    await interaction.reply(
+      `
+      hey babe, welcome to this channel. \n \
+      I made a bot for us to log our daily tasks more easily. \n \
+      I'm starting to feel like this is kind of silly because I spent the nights of this whole week on it. \n \
+      Try typing **/log**, enter the information, open the Google Sheet, and see what happens. \n \
+      You see this in English because I don't want to be 这么客气的跟你说这些话. \n \
+      Tell me your thoughts, and we can make it better!
+      `
+    );
   },
 };
